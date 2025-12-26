0a4b6d3ff5fdac7d8f21d0c77b8c0ad2d40816f0
Pravat Patra <pravatpatra1997@gmail.com>
2025-12-26 23:53:12 +0530

    git initialized & requirements are installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the full phrase project for prediciton of swiggy food delivery prediction.',
+    author='Pravat Patra',
+    license='',
+)
